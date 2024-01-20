034aff60d7e25af1b974b75bc6a76e56e516a513
Nithin Rajkumar <nithin-r@industryapps.net>
2024-01-20 13:36:56 +0530

    Modules

---
+++ Day2/modules.py
@@ -0,0 +1,13 @@
+import math_operations
+
+print(math_operations.add(4,5)) # importng function from math_operations to sum two numbers
+print(math_operations.sub(6,2))
+print(math_operations.mul(3,4))
+print(math_operations.mul(8,2))
+print(math_operations.power(2,3))
+
+result_additional_1 = math_operations.add(2, 8)
+print("Additional Operation 1 Result:", result_additional_1)
+
+result_additional_2 = math_operations.subtract(12, 5)
+print("Additional Operation 2 Result:", result_additional_2)
