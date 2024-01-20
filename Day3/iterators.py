5241d0134bf5821f58483ef5acedb7fa7498641f
Nithin Rajkumar <nithin-r@industryapps.net>
2024-01-20 15:18:12 +0530

    iterators and generators

---
+++ Day3/iterators.py
@@ -0,0 +1,8 @@
+file = open("C:\\Users\\NithinRajkumar\\Desktop\\skill_captain_python_advanced\\Day3\\test.txt","r")
+
+print(next(file))
+print(next(file))
+my_file = iter(file)
+print(next(file))
+
+
